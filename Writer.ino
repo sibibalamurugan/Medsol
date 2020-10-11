a9e6efa81a8316c5d49069e11456d6a6f6dec610
sibibalamurugan <72707139+sibibalamurugan@users.noreply.github.com>
2020-10-12 00:19:17 +0530

    Add files via upload

---
+++ Writer.ino
@@ -0,0 +1,225 @@
+#include <SPI.h>//include the SPI bus library
+#include <MFRC522.h>//include the RFID reader library
+
+#define SS_PIN 10  //slave select pin
+#define RST_PIN 5  //reset pin
+MFRC522 mfrc522(SS_PIN, RST_PIN);        // instatiate a MFRC522 reader object.
+MFRC522::MIFARE_Key key;
+
+String inputString = "";         // a string to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
+String commandString = "";
+
+boolean isConnected = false;
+
+
+void setup() {
+ 
+  Serial.begin(9600);
+           // Initialize serial communications with the PC
+        SPI.begin();               // Init SPI bus
+        mfrc522.PCD_Init();        // Init MFRC522 card (in case you wonder what PCD means: proximity coupling device)
+        Serial.println("Scan a MIFARE Classic card");
+        
+        // Prepare the security key for the read and write functions - all six key bytes are set to 0xFF at chip delivery from the factory.
+        // Since the cards in the kit are new and the keys were never defined, they are 0xFF
+        // if we had a card that was programmed by someone else, we would need to know the key to be able to access it. This key would then need to be stored in 'key' instead.
+ 
+        for (byte i = 0; i < 6; i++) {
+                key.keyByte[i] = 0xFF;//keyByte is defined in the "MIFARE_Key" 'struct' definition in the .h file of the library
+        }
+  pinMode(8,OUTPUT);    
+  pinMode(7,OUTPUT);   
+}
+
+
+int block=2;//this is the block number we will write into and then read. Do not write into 'sector trailer' block, since this can make the block unusable.
+String tempo;                          
+byte med1[16] = {"_______________"};//an array with 16 bytes to be written into one of the 64 card blocks is defined!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+byte med2[16] = {"_______________"};
+byte med3[16] = {"_______________"};
+byte naem[16] = {"_______________"};
+byte age[16]  = {"_______________"};
+byte vlai1[16] = {"_______________"};
+byte vlai2[16] = {"_______________"};
+byte vlai3[16] = {"_______________"};
+
+byte readbackblock[18];//This array is used for reading out a block. The MIFARE_Read method requires a buffer that is at least 18 bytes to hold the 16 bytes of a block.
+int z=0;
+
+
+
+
+void loop() {
+
+if(stringComplete)
+{ 
+  stringComplete = false;
+  getCommand();
+  if(commandString.equals("TEXT"))
+  {   digitalWrite(8,HIGH);
+  
+    String text = getTextToPrint();
+//sssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+
+int lop=0;
+while(lop==0)
+{
+ lop=1;
+    // Look for new cards (in case you wonder what PICC means: proximity integrated circuit card)
+ if ( ! mfrc522.PICC_IsNewCardPresent()) {//if PICC_IsNewCardPresent returns 1, a new card has been found and we continue
+    lop=0;//if it did not find a new card is returns a '0' and we return to the start of the loop
+  }
+
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {//if PICC_ReadCardSerial returns 1, the "uid" struct (see MFRC522.h lines 238-45)) contains the ID of the read card.
+    lop=0;//if it returns a '0' something went wrong and we return to the start of the loop
+  }
+
+}
+         digitalWrite(7,HIGH);
+     //EDIT done here to sample code - to enter a string into the 2nd block of RFID 
+        int n=text.length();
+ 
+         char exec[64]={'_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_'};
+         strcpy(exec, text.c_str()); 
+ 
+         char code='-';
+         for(unsigned int i = 0,j=0,k=0;k<8; i++,j++)
+         {if(exec[j]==code)
+         {k++;
+         i=0;
+         }    
+         else if(k==0)
+         naem[i] = (byte)exec[j];  
+         else if(k==1)
+         age[i] = (byte)exec[j];  
+         else if(k==2)
+         med1[i] = (byte)exec[j]; 
+         else if(k==3)
+         vlai1[i] = (byte)exec[j]; 
+         else if(k==4)
+         med2[i] = (byte)exec[j]; 
+         else if(k==5)
+         vlai2[i] = (byte)exec[j];
+         else if(k==6)
+         med3[i] = (byte)exec[j]; 
+         else if(k==7)
+         vlai3[i] = (byte)exec[j];                                               
+         }
+                  
+         /*****************************************writing block on the card**********************************************************************/
+         
+         writeBlock(2,naem);
+         writeBlock(13,age);
+         writeBlock(4,med1);
+         writeBlock(5,vlai1);
+         writeBlock(6,med2);
+         writeBlock(12,vlai2);
+         writeBlock(8,med3);
+         writeBlock(9,vlai3);
+  }
+ inputString = ""; 
+}}
+
+void getCommand()
+{
+  if(inputString.length()>0)
+  {
+     commandString = inputString.substring(1,5);
+  }
+}
+
+
+String getTextToPrint()
+{
+  String value = inputString.substring(5,inputString.length()-2);
+  return value;
+}
+
+
+
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag
+    // so the main loop can do something about it:
+    if (inChar == '\n') {
+      stringComplete = true;
+    }
+  }
+}
+int writeBlock(int blockNumber, byte arrayAddress[]) 
+{
+  //this makes sure that we only write into data blocks. Every 4th block is a trailer block for the access/security info.
+  int largestModulo4Number=blockNumber/4*4;
+  int trailerBlock=largestModulo4Number+3;//determine trailer block for the sector
+  if (blockNumber > 2 && (blockNumber+1)%4 == 0){Serial.print(blockNumber);Serial.println(" is a trailer block:");return 2;}//block number is a trailer block (modulo 4); quit and send error code 2
+  Serial.print(blockNumber);
+  Serial.println(" is a data block:");
+  
+  /*****************************************authentication of the desired block for access***********************************************************/
+  byte status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, trailerBlock, &key, &(mfrc522.uid));
+  //byte PCD_Authenticate(byte command, byte blockAddr, MIFARE_Key *key, Uid *uid);
+  //this method is used to authenticate a certain block for writing or reading
+  //command: See enumerations above -> PICC_CMD_MF_AUTH_KEY_A = 0x60 (=1100000),    // this command performs authentication with Key A
+  //blockAddr is the number of the block from 0 to 15.
+  //MIFARE_Key *key is a pointer to the MIFARE_Key struct defined above, this struct needs to be defined for each block. New cards have all A/B= FF FF FF FF FF FF
+  //Uid *uid is a pointer to the UID struct that contains the user ID of the card.
+  if (status != MFRC522::STATUS_OK) {
+         Serial.print("PCD_Authenticate() failed: ");
+         Serial.println(mfrc522.GetStatusCodeName(status));
+         return 3;//return "3" as error message
+  }
+  //it appears the authentication needs to be made before every block read/write within a specific sector.
+  //If a different sector is being authenticated access to the previous one is lost.
+
+
+  /*****************************************writing the block***********************************************************/
+        
+  status = mfrc522.MIFARE_Write(blockNumber, arrayAddress, 16);//valueBlockA is the block number, MIFARE_Write(block number (0-15), byte array containing 16 values, number of bytes in block (=16))
+  //status = mfrc522.MIFARE_Write(9, value1Block, 16);
+  if (status != MFRC522::STATUS_OK) {
+           Serial.print("MIFARE_Write() failed: ");
+           Serial.println(mfrc522.GetStatusCodeName(status));
+           return 4;//return "4" as error message
+  }
+  Serial.println("block was written");
+}
+
+
+int readBlock(int blockNumber, byte arrayAddress[]) 
+{
+  int largestModulo4Number=blockNumber/4*4;
+  int trailerBlock=largestModulo4Number+3;//determine trailer block for the sector
+
+  /*****************************************authentication of the desired block for access***********************************************************/
+  byte status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, trailerBlock, &key, &(mfrc522.uid));
+  
+  if (status != MFRC522::STATUS_OK) {
+         Serial.print("PCD_Authenticate() failed (read): ");
+         Serial.println(mfrc522.GetStatusCodeName(status));
+         return 3;//return "3" as error message
+  } 
+  
+       
+  byte buffersize = 18;//we need to define a variable with the read buffer size, since the MIFARE_Read method below needs a pointer to the variable that contains the size... 
+  status = mfrc522.MIFARE_Read(blockNumber, arrayAddress, &buffersize);//&buffersize is a pointer to the buffersize variable; MIFARE_Read requires a pointer instead of just a number
+  if (status != MFRC522::STATUS_OK) {
+          Serial.print("MIFARE_read() failed: ");
+          Serial.println(mfrc522.GetStatusCodeName(status));
+          return 4;//return "4" as error message
+  }
+  Serial.println("block was read");
+}
+
+
+
+
+
+
+
+
